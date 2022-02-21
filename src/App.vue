ed74d1dc53d15e2147973c62f12cc5277a6a3f5f
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-21 23:40:43 +0300

    Fix css fully responsive and get position info vue
    Made css responsive and getting postion information from vue js ref
    (torba and position) . Delete this.position

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
   <div id="powt"></div>
   <div class="me" id="me" ref="myme"></div>
   <button @keydown="keybordcatcher($event)" clas="btn">start</button>
-  <div class="kumtorbası"></div>
+  <div class="kumtorbası" ref="torba"></div>
   <div class="beko"></div>
   <div class="bam" ref="bambam"><img src="./assets/bam33.png" /></div>
+  <div class="textt"><h1>Just desktop</h1></div>
 </template>
 
 <script>
@@ -16,14 +17,18 @@ export default {
   name: "App",
 
   data() {
-    return { position: 600, play: false };
+    return { play: false };
   },
 
   methods: {
     keybordcatcher(e) {
       let me = this.$refs.myme;
       let mybam = this.$refs.bambam;
-      console.log(this.position);
+      let torba = this.$refs.torba.getBoundingClientRect().left;
+      let position = this.$refs.myme.getBoundingClientRect().left;
+      console.log(torba + " " + this.position);
+
+      // console.log(this.position);
 
       if (this.play == false) {
         this.play = true;
@@ -32,26 +37,26 @@ export default {
       }
 
       if (e.keyCode == "39") {
-        console.log(39);
-        this.position = Ben.walkForward(this.position, me);
+        // console.log(39);
+        this.position = Ben.walkForward(position, me);
       }
 
       if (e.keyCode == "37") {
         console.log(37);
-        this.position = Ben.walkBack(this.position, me);
+        this.position = Ben.walkBack(position, me);
       }
 
       if (e.keyCode == "81") {
-        console.log(81);
+        // console.log(81);
         Ben.punchL(me);
       }
 
       if (e.keyCode == "69") {
-        console.log(69);
+        // console.log(69);
         Ben.punchR(me);
       }
-      if (this.position == 900) {
-        console.log("his");
+      if (position + 200 > torba && position + 200 < torba + 100) {
+        // console.log("his");
         setTimeout(() => {
           mybam.style = `opacity:1;`;
         }, 150);
@@ -65,7 +70,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -74,28 +79,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.textt {
+  opacity: 0;
+}
 body {
   background-image: url(./assets/ring3.jpg);
   background-repeat: no-repeat;
   background-size: cover;
 }
 .me {
-  object-fit: cover;
+  background-size: contain;
   position: absolute;
-  width: 500px;
-  height: 700px;
+  width: 22%;
+  height: 50%;
 
-  left: 600px;
-  top: 300px;
+  left: 32%;
+  top: 30%;
   background-image: url(./assets/RESV1.png);
   background-repeat: no-repeat;
 }
 .me2 {
   position: absolute;
-  width: 500px;
-  height: 600px;
+  background-size: contain;
 
-  left: 600px;
+  width: 22%;
+  height: 50%;
+  left: 32%;
+  top: 30%;
   animation-name: leftpunch;
   animation-duration: 200ms;
   background-repeat: no-repeat;
@@ -111,10 +121,12 @@ body {
 
 .me3 {
   position: absolute;
-  width: 500px;
-  height: 600px;
+  background-size: contain;
 
-  left: 600px;
+  width: 22%;
+  height: 50%;
+  left: 32%;
+  top: 30%;
   animation-name: rightpunch;
   animation-duration: 200ms;
   background-repeat: no-repeat;
@@ -126,37 +138,74 @@ body {
     background-image: url(./assets/RES3.png);
   }
 }
-#powt {
-  font-size: 200px;
-  position: absolute;
-  left: 50%;
-}
 
 .kumtorbası {
   background-image: url(./assets/kumtorbası.png);
+  background-size: contain;
+
   position: absolute;
   z-index: -1;
-  width: 300px;
-  height: 550px;
-  top: 100px;
-  left: 1200px;
+  width: 20%;
+  height: 60%;
+  top: 13%;
+  left: 60%;
   background-repeat: no-repeat;
 }
 .beko {
   background-image: url(./assets/bekotra.png);
+  background-size: contain;
 
   z-index: -1;
   position: absolute;
-  width: 250px;
-  height: 550px;
+  width: 13%;
+  height: 20%;
   background-repeat: no-repeat;
-  top: 320px;
-  left: 1270px;
+  top: 40%;
+  left: 65%;
 }
 .bam {
   position: absolute;
+
   opacity: 0;
-  top: 420px;
-  left: 1250px;
+  top: 40%;
+  left: 65%;
+}
+
+@media screen and (min-device-width: 1200px) and (max-device-width: 1600px) and (-webkit-min-device-pixel-ratio: 1) {
+  .me {
+    top: 40%;
+  }
+  .me2 {
+    top: 40%;
+  }
+  .me3 {
+    top: 40%;
+  }
+}
+.bam {
+  top: 40%;
+}
+@media only screen and (max-width: 768px) and (-webkit-min-device-pixel-ratio: 1) {
+  .me {
+    opacity: 0;
+  }
+  .me2 {
+    opacity: 0;
+  }
+  .me3 {
+    opacity: 0;
+  }
+  .bam {
+    opacity: 0;
+  }
+  .kumtorbası {
+    opacity: 0;
+  }
+  .textt {
+    opacity: 1;
+  }
+  .beko {
+    opacity: 0;
+  }
 }
 </style>
